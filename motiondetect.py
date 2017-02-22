7214beca56a7410edcc67bebbac46f29a314ead9
Apache <apache@tencent.com>
2017-02-22 13:21:22 +0000

    up

--- motiondetect.py
+++ motiondetect.py
@@ -1,10 +1,19 @@
 #-*- coding: utf-8 -*-
 import cv2
 import time
+import json
+import urllib2
+import urllib
 
-motion_limit_value = 130000
+wx_server_url = "http://115.159.213.156/motion"
+motion_limit_value = 123000
 limit_value_count = 5
 
+def on_motion_detected():
+    print "motion detected"
+    res = urllib2.urlopen(wx_server_url + "?motion_str=19", timeout=5)
+    print res.read()
+    
 def motion_detect():
     img1 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
     img2 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
@@ -18,10 +27,10 @@ def motion_detect():
         value = cv2.countNonZero(cv2.bitwise_and(d1, d2))
         print value
         if value >= motion_limit_value:
-            t = 0.5
+            t = 0.2
             count += 1
             if count > limit_value_count:
-                print "Motion!"
+                on_motion_detected()
                 count = 0
         else:
             t = 1
@@ -42,6 +51,7 @@ if __name__ == "__main__":
         
     try:
         motion_detect()
+        #on_motion_detected()
     except KeyboardInterrupt as e:
         print "Bay"
         
